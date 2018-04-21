7935ed2b19bdf5f44dfb8735e8f031d197207c86
zhujiagang <1058776021@qq.com>
2018-04-21 18:41:20 +0800

    new

--- ucf_scripts_split_1/extract_optical_flow.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env bash
-
-SRC_FOLDER=$1
-OUT_FOLDER=$2
-NUM_WORKER=$3
-FLOW_TYPE=$4
-echo "Extracting optical flow from videos in folder: ${SRC_FOLDER}"
-python tools/build_of.py ${SRC_FOLDER} ${OUT_FOLDER} --num_worker ${NUM_WORKER} --flow_type ${FLOW_TYPE} --new_width 340 --new_height 256 2>local/errors.log
-echo "here"
--- ucf_scripts_split_2/build_file_list.sh
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env bash
-
-DATASET=$1
-FRAME_PATH=$2
-
-python tools/build_file_list.py ${DATASET} ${FRAME_PATH} --shuffle
